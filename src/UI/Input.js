36fd608bd3d23caec2c1e7adaec5d1d5971fea11
kasra <macbookappleidkasra@gmail.com>
2023-07-03 22:39:20 +0330

    Add some Styles

--- src/UI/Input.js
+++ src/UI/Input.js
@@ -2,11 +2,11 @@ import { Fragment } from "react";
 
 const Input = (props) => {
   const styles =
-    "p-4  border-2 border-solid border-white rounded-3xl mt-5 " +
-    " cursor-pointer focus:border-green-700 focus:text-green-700 font-semibold";
+    "p-4  border-2 border-solid border-white rounded-3xl mt-5 focus:border-green-700" +
+    " cursor-pointer active:border-green-700  active:text-green-700 font-semibold focus:text-green-700 ";
   const { myValue } = props;
   return (
-    <Fragment className=" font">
+    <Fragment>
       {myValue.map((item) => (
         <input
           key={item.id}
